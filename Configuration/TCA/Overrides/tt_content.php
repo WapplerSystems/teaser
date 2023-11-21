4c476709978c1d0a23ad686ca2ffc83848b35296
Sven Wappler <info@wappler.systems>
2023-11-21 16:38:45 +0100

    updates

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,40 +2,51 @@
 defined('TYPO3') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Teaser',
+    'teaser2',
     'List',
     'Teaser'
 );
 
 $additionalColumns = [
+    'tx_teaser2_items' => [
+        'exclude' => 0,
+        'label' => 'LLL:EXT:teaser2/Resources/Private/Language/locallang.xlf:tt_content.items',
+        'config' => [
+            'type' => 'inline',
+            'foreign_table' => 'tx_teaser2_domain_model_item',
+            'foreign_field' => 'content_uid',
+            'foreign_label' => 'title',
+            'foreign_sortby' => 'sorting',
+            'maxitems' => '100',
+            'appearance' => [
+                #'collapseAll' => 0, // Auskommentieren, da es sonst immer als true interpretiert wird
+                'expandSingle' => true,
+                'newRecordLinkAddTitle' => 1,
+                'newRecordLinkPosition' => 'both',
+                'showAllLocalizationLink' => true,
+                'showPossibleLocalizationRecords' => true,
+            ],
+            'behaviour' => [
+                'localizationMode' => 'select',
+            ],
+            'overrideChildTca' => [
+                'types' => [
+                    '1' => [
+                        'showitem' => 'title, subtitle, sys_language_uid, media, text_position, link, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,starttime, endtime'
+                    ],
+                ]
+            ]
+        ]
+    ],
     'tx_teaser2_layout' => [
         'exclude' => true,
         'label' => 'LLL:EXT:teaser2/Resources/Private/Language/locallang_db.xlf:tt_content.layout',
         'config' => [
             'type' => 'select',
             'renderType' => 'selectSingle',
-            'default' => 'ease-in-out',
+            'default' => 'grid1',
             'items' => [
-                ['linear', 'linear'],
-                ['ease', 'ease'],
-                ['ease-in', 'ease-in'],
-                ['ease-out', 'ease-out'],
-                ['ease-in-out', 'ease-in-out'],
-                ['ease-in-back', 'ease-in-back'],
-                ['ease-out-back', 'ease-out-back'],
-                ['ease-in-out-back', 'ease-in-out-back'],
-                ['ease-in-sine', 'ease-in-sine'],
-                ['ease-out-sine', 'ease-out-sine'],
-                ['ease-in-out-sine', 'ease-in-out-sine'],
-                ['ease-in-quad', 'ease-in-quad'],
-                ['ease-out-quad', 'ease-out-quad'],
-                ['ease-in-out-quad', 'ease-in-out-quad'],
-                ['ease-in-cubic', 'ease-in-cubic'],
-                ['ease-out-cubic', 'ease-out-cubic'],
-                ['ease-in-out-cubic', 'ease-in-out-cubic'],
-                ['ease-in-quart', 'ease-in-quart'],
-                ['ease-out-quart', 'ease-out-quart'],
-                ['ease-in-out-quart', 'ease-in-out-quart'],
+                ['Layout 1', 'grid1'],
             ],
         ]
     ],
@@ -44,4 +55,4 @@ $additionalColumns = [
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
 
 
-$GLOBALS['TCA']['tt_content']['palettes']['teaser2'] = ['showitem' => 'imagecols_xs,imagecols_sm,imagecols_md,imagecols_lg,imagecols_xl,imagecols_xxl'];
+$GLOBALS['TCA']['tt_content']['palettes']['teaser2'] = ['showitem' => 'tx_teaser2_items, --linebreak--, tx_teaser2_layout'];
