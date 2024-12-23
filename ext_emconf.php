ed24ab2ed4d661c038b108530513a3b1f0b54b9a
Sven Wappler <wappler@wappler.systems>
2024-12-23 23:34:28 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF['teaser2'] = [
     'author_email' => 'typo3@wappler.systems',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '12.0.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
