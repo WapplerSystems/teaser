60d2655f5d223362a94c13e5e9f5d80265ba1d7d
Sven Wappler <wappler@wappler.systems>
2024-12-24 00:11:04 +0100

    improve description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,11 +2,11 @@
 
 $EM_CONF['teaser2'] = [
     'title' => 'Teaser',
-    'description' => '',
+    'description' => 'This extension displays images and videos in a responsive grid layout.',
     'category' => 'plugin',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3@wappler.systems',
-    'state' => 'alpha',
+    'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'version' => '13.0.0',
     'constraints' => [
