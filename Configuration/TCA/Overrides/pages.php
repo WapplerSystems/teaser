a828ac3e6760d5f5208f8002ead06c7c1c66dafe
Sven Wappler <wappler@wappler.systems>
2025-04-09 12:00:04 +0200

    remove old registrations

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'teaser2',
-    'Configuration/TsConfig/Page/ContentElementWizard.tsconfig',
-    'Enable teaser2 in wizard'
-);
--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-defined('TYPO3') || die();
-
-ExtensionManagementUtility::addStaticFile('teaser2', 'Configuration/TypoScript', 'Teaser');
